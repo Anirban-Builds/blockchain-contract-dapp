95b52b8363d8a6e4724ec14ebff92dbff65d2bd1
Anirban0011 <bhattandbhatt.999@gmail.com>
2026-05-10 16:28:10 +0530

    upd

--- src/scripts/hf-space.js
+++ src/scripts/hf-space.js
@@ -1,7 +1,5 @@
 import { createRepo } from '@huggingface/hub'
 
-console.log("Token being used:", process.env.HF_TOKEN?.slice(0, 5))
-
 await createRepo({
   repo: { type: 'space', name: `Anirban0011/${process.env.REPO}` },
   token: process.env.HF_TOKEN,
