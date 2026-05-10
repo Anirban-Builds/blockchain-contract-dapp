5f97c557a660971ed45d54cde9a7f22ed47d83ab
Anirban0011 <bhattandbhatt.999@gmail.com>
2026-05-10 22:44:32 +0530

    upd

--- src/scripts/hf-space.js
+++ src/scripts/hf-space.js
@@ -2,7 +2,7 @@ import { createRepo } from '@huggingface/hub'
 
 await createRepo({
   repo: { type: 'space', name: `Anirban0011/${process.env.REPO}` },
-  token: `${process.env.HF_TOKEN}`,
+  token: process.env.HF_TOKEN,
   spaceSDK: 'docker',
   ifExists: 'skip'
 })
