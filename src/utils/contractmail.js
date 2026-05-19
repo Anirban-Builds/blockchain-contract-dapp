e8c38916055aa369eb7fea476f35273137843e5b
Anirban0011 <bhattandbhatt.999@gmail.com>
2026-05-19 18:16:43 +0530

    upd

--- src/utils/contractmail.js
+++ src/utils/contractmail.js
@@ -3,9 +3,8 @@ import { MODE } from "../../constants.js"
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
-console.log(MODE)
-
 const sendMail = async(to, walletid, URL, filename)=>{
+    console.log('sendMail MODE:', MODE)  // add this
     return await resend.emails.send({
         from : "Anirban Builds <sign-contract@anirbanbuilds.online>",
         to,
